b0c0ccd5bdb128e548beb927055dce85937674b1
BobM035 <BobM035@gmail.com>
2015-09-21 01:06:43 -0400

    First Draft
    Correct results but supporting files haven't been edited yet.

--- run_analysis.R
+++ run_analysis.R
@@ -17,21 +17,32 @@ cols<-gsub('\\)','',cols)
 cols<-gsub('\\,','',cols)
 cols<-gsub('-','',cols)
 cols<-gsub('\\,','',cols)
+## Alternate method not implemented
+## make.names(cols)
 ## Read in Activity Labels
 alabs<-read.table("activity_labels.txt", stringsAsFactors=FALSE)
-## Rename Columns
+## Rename Columns 
 colnames(Xtest)<-cols
 colnames(Xtrain)<-cols
-subtest<-rename(subtest, Subject = V1)
-subtrain<-rename(subtrain, Subject = V1)
-ytest<-rename(ytest,Activity = V1)
-ytrain<-rename(ytrain,Activity = V1)
-## merge columns
+subtest<-rename(subtest, subject = V1)
+subtrain<-rename(subtrain, subject = V1)
+ytest<-rename(ytest, activity = V1)
+ytrain<-rename(ytrain, activity = V1)
+## merge columns to add subject and activity data
 bigtest<-cbind(subtest,Xtest)
 bigtest<-cbind(ytest,bigtest)
 bigtrain<-cbind(subtrain,Xtrain)
 bigtrain<-cbind(ytrain,bigtrain)
-## Merge Test and Train
+## Merge Test and Train data sets
 combodata<-rbind(bigtrain,bigtest)
 ## Change Activity # to Activity Label
 combodata[,1]<-alabs[combodata[,1],2]
+## Extract columns dealing with mean and standard deviation
+mstd1<-combodata[,grep("activity|subject|std|mean",names(combodata))]
+# Remove columns with meanFreq
+mstd<-mstd1[,!grepl('F',names(mstd1))]
+# Create Tidy Data set
+TidyData<-group_by(mstd,activity,subject) %>% summarise_each("mean") 
+# Write Tidy Data set to disk
+write.table(TidyData,file="TidyData.txt", row.names = FALSE)
+
